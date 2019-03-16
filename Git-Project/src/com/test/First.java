18822dae38b40e3601c60a892ba2665644d53da5
MKumar20 <MKumar20@HIPUNL51909.ad.harman.com>
2019-03-16 18:06:54 +0530

    Test Commit

---
+++ Git-Project/src/com/test/First.java
@@ -0,0 +1,12 @@
+package com.test;
+public class First {
+   public static void main(String[] args) {
+	System.out.println("This is main method");
+	System.out.println("We are using here Java 8 new features like:");
+	System.out.println("Functional Interface");
+	System.out.println("Lambda Expression");
+	System.out.println("Stream API");
+	System.out.println("Filter");
+	System.out.println("forEach Method");
+}
+}
